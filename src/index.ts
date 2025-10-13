daeb141444fa55ea80e703031187ad7c9f77f1f4
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2025-10-13 21:18:38 +0000

    csv processing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
-import { generate } from 'csv'
+import { generate, parse } from 'csv'
+import fs from "node:fs";
 
-const main = () => {
-    generate({
-        columns: ["int", "bool"],
-        length: 2,
-    }).pipe(process.stdout);
+const __dirname = new URL(".", import.meta.url).pathname;
+
+const getFileReader = async (fileName: string) => {
+    const readStream = fs.createReadStream(`${__dirname}/../import/${fileName}`)
+    return readStream
+}
+
+const main = async () => {
+    const allFiles = fs.readdirSync(`${__dirname}/../import`)
+    const csvFiles = allFiles.filter((file) => file.endsWith('.csv'))
+    console.log('Number of files to process:', csvFiles.length)
+
+
+    for (const file of csvFiles) {
+        console.log('Processing file:', file)
+        const fileStream = await getFileReader(file)
+        const parser = fileStream.pipe(parse())
+
+        // for await (const record of parser) {
+        //     console.log(record)
+        // }
+    }
 }
 
 main()
+    .then(() => {
+        console.log('Done')
+    })
+    .catch((err) => {
+        console.error('Error:', err)
+    })
