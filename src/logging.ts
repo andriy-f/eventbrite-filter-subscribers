326b0a62c059efc91cb015591d2a5df82c71ba0c
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2025-10-19 15:21:13 +0000

    pino logger infra

---
+++ src/logging.ts
@@ -0,0 +1,17 @@
+import pino from 'pino';
+
+// const logLevels = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'] as const;
+
+// const isLogLevel = (value: any): value is typeof logLevels[number] => {
+//     return logLevels.includes(value);
+// };
+
+export const logger = pino({
+    // level: process.env.LOG_LEVEL && isLogLevel(process.env.LOG_LEVEL) ? process.env.LOG_LEVEL : 'debug',
+    level: process.env.LOG_LEVEL ?? 'debug',
+    // formatters: {
+    //     level(label) {
+    //         return { level: label };
+    //     },
+    // },
+});
